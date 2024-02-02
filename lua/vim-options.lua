8c7b1de41662179aba5d299c67dc1dab3f03a19e
Vishen Gounden <vgounden@gmail.com>
2024-02-02 15:57:53 +0200

    formatting changes

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -11,5 +11,25 @@ vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+vim.keymap.set({'n', 'v'}, '<Space>', '<Nop>', { silent = true })
+
+
 vim.wo.number = true
 vim.o.relativenumber = true
+
+vim.o.clipboard = "unnamedplus"
+
+vim.o.undofile =  true
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.wo.signcolumn = 'yes'
+
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+
+vim.o.completeopt = 'menuone,noselect'
+
+vim.o.termguicolors = true
