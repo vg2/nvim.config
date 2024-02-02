5e3254bc1f6781d3b37ff7d2ffe662d3400fdaf8
Vishen Gounden <vgounden@gmail.com>
2024-02-02 12:26:25 +0200

    add null-s and formatters

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,28 +1,54 @@
 return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "tsserver" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.tsserver.setup({})
+      local opts = {}
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+      vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+    end,
+  },
+  {
     {
-        "williamboman/mason.nvim",
-        config = function () 
-            require("mason").setup()
-        end
+      "nvimtools/none-ls.nvim",
+      config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+          sources = {
+            null_ls.builtins.formatting.stylua,
+            null_ls.builtins.diagnostics.eslint,
+          },
+        })
+        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+      end,
     },
-    {
-         "williamboman/mason-lspconfig.nvim",
-        config = function () 
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "tsserver" }
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function () 
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.tsserver.setup({})
-            local opts = { }
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-            vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-        end
-    }
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    config = function()
+      local mason_null_ls = require("mason-null-ls")
+      mason_null_ls.setup({
+        ensure_installed = nil,
+        automatic_installation = true,
+      })
+    end,
+  },
 }
