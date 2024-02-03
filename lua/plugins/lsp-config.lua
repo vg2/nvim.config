18e26cabed54ba17ce4fc9f8c143ad8c50b4b38f
Vishen Gounden <vgounden@gmail.com>
2024-02-03 22:07:25 +0200

    formatting

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,60 +1,60 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver", "omnisharp", "azure_pipelines_ls", "rust_analyzer" },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.tsserver.setup({
-        capabilities = capabilities
-      })
-      local opts = {}
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-      vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-    end,
-  },
-  {
-    {
-      "nvimtools/none-ls.nvim",
-      config = function()
-        local null_ls = require("null-ls")
-        null_ls.setup({
-          sources = {
-            null_ls.builtins.formatting.stylua,
-            null_ls.builtins.formatting.prettierd,
-            null_ls.builtins.formatting.eslint_d
-          },
-        })
-        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-      end,
-    },
-  },
-  {
-    "jay-babu/mason-null-ls.nvim",
-    config = function()
-      local mason_null_ls = require("mason-null-ls")
-      mason_null_ls.setup({
-        ensure_installed = nil,
-        automatic_installation = true,
-      })
-    end,
-  },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "tsserver", "omnisharp", "azure_pipelines_ls", "rust_analyzer" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+			local opts = {}
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+		end,
+	},
+	{
+		{
+			"nvimtools/none-ls.nvim",
+			config = function()
+				local null_ls = require("null-ls")
+				null_ls.setup({
+					sources = {
+						null_ls.builtins.formatting.stylua,
+						null_ls.builtins.formatting.prettierd,
+						null_ls.builtins.formatting.eslint_d,
+					},
+				})
+				vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+			end,
+		},
+	},
+	{
+		"jay-babu/mason-null-ls.nvim",
+		config = function()
+			local mason_null_ls = require("mason-null-ls")
+			mason_null_ls.setup({
+				ensure_installed = nil,
+				automatic_installation = true,
+			})
+		end,
+	},
 }
