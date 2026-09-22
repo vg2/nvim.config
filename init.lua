b848aaa8cd81a908e96b1a1cd1aae32e8d47fc2b
Vishen Gounden <vishen.gounden@entelect.co.za>
2026-09-22 12:29:31 +0200

    feat: support wsl2 to windows clipboard handling

--- init.lua
+++ init.lua
@@ -119,10 +119,28 @@ do
   vim.o.showmode = false
 
   -- Sync clipboard between OS and Neovim.
+  -- Configure clipboard provider for WSL2
+  if vim.fn.has('wsl') == 1 then
+    vim.g.clipboard = {
+      name = 'WslClipboard',
+      copy = {
+        ['+'] = 'clip.exe',
+        ['*'] = 'clip.exe',
+      },
+      paste = {
+        ['+'] = { 'powershell.exe', '-NoLogo', '-NoProfile', '-Command', '[Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))' },
+        ['*'] = { 'powershell.exe', '-NoLogo', '-NoProfile', '-Command', '[Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))' },
+      },
+      cache_enabled = 0,
+    }
+  end
+
   --  Schedule the setting after `UiEnter` because it can increase startup-time.
   --  Remove this option if you want your OS clipboard to remain independent.
   --  See `:help 'clipboard'`
-  vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
+  vim.schedule(function()
+    vim.o.clipboard = 'unnamedplus'
+  end)
 
   -- Enable break indent
   vim.o.breakindent = true
